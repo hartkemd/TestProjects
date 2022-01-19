d6c7eed9ff23ca8ae3c1a8baf0d48eaff85b9c34
hartkemd <hartkemd@gmail.com>
2022-01-18 21:45:14 -0500

    Added variables test projects.

---
+++ VariablesTestApp/VariablesTest/Program.cs
@@ -0,0 +1,26 @@
+﻿
+string firstName = "Mark";
+string lastName = "Hartkemeyer";
+string fullName = $"{firstName} {lastName}"; // string interpolation
+Console.WriteLine($"My name is: {fullName}");
+
+string filePath = "C:\\Temp\\sample.csv"; // using backslash as an escape character
+Console.WriteLine($"First file path: {filePath}");
+string betterFilePath = @"C:\Temp\sample.csv"; // string literal; this can be copied and used elsewhere
+Console.WriteLine($"Second file path: {betterFilePath}");
+
+int age = 0;
+age = age + 5;
+age += 25;
+Console.WriteLine($"My age is: {age}");
+
+double feetTall;
+feetTall = 5.9;
+Console.WriteLine($"I am {feetTall} feet tall");
+
+decimal priceOfChips = 2.49M; // use decimal for money
+Console.WriteLine($"The price of a bag of chips is: ${priceOfChips}");
+
+Console.WriteLine();
+Console.WriteLine("Press enter to continue...");
+Console.ReadLine();
---
+++ VariablesTestApp3/VariablesTest/Program.cs
@@ -0,0 +1,15 @@
+﻿byte test = 255;
+Console.WriteLine($"test = {test}");
+sbyte test2 = -100; // signed byte
+Console.WriteLine($"test2 = {test2}");
+
+string numberOfDaysInAWorkWeek = "5";
+string numberOfDaysInAWeekend = "2";
+Console.WriteLine($"Wrong number of days in a week: { numberOfDaysInAWorkWeek + numberOfDaysInAWeekend }"); // wrong
+
+int intNumberOfDaysInAWorkWeek = 5;
+int intNumberOfDaysInAWeekend = 2;
+Console.WriteLine($"Number of days in a week: { intNumberOfDaysInAWorkWeek + intNumberOfDaysInAWeekend }"); // right
+
+Console.WriteLine("Press enter to continue...");
+Console.ReadLine();
