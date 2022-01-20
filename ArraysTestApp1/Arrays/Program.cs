4bd06baf69843f0f535f47f32285f2caaef2fd3b
hartkemd <hartkemd@gmail.com>
2022-01-19 21:18:06 -0500

    Added arrays test projects.

---
+++ ArraysTestApp1/Arrays/Program.cs
@@ -0,0 +1,16 @@
+﻿
+//string[] firstNames = new string[10];
+//firstNames[0] = "Mark";
+//firstNames[1] = "Tim";
+//firstNames[3] = "Carl";
+
+//Console.WriteLine($"My array has {firstNames[0]}, {firstNames[1]}, {firstNames[3]}");
+
+string data = "Mark,Tim,Carl";
+string[] firstNames = data.Split(',');
+
+Console.WriteLine($"The third item in the array is: {firstNames[2]}");
+
+Console.WriteLine();
+Console.WriteLine("Press enter to continue...");
+Console.ReadLine();
