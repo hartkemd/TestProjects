9b3cbfc62f316b328cc1c0b0585dc43b5e6cfd5b
hartkemd <hartkemd@gmail.com>
2022-01-21 18:46:26 -0500

    Added for loops test projects.

---
+++ ForLoopsTestApp1/ForLoopsTest/Program.cs
@@ -0,0 +1,18 @@
+﻿
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine();
+
+string[] firstNames = new string[] { "Mark", "Tim", "Sue", "Bob" };
+
+for (int i = 0; i < firstNames.Length; i++)
+{
+    Console.WriteLine(firstNames[i]);
+}
+
+Console.WriteLine();
+Console.WriteLine("Press enter to continue...");
+Console.ReadLine();
---
+++ ForLoopsTestApp3/ForLoopsTest/Program.cs
@@ -0,0 +1,18 @@
+﻿
+for (int i = 100; i > 0; i -= 3)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine();
+
+List<bool> switchPositions = new List<bool>() { true, true, false, false, true };
+
+for (int i = 0; i < switchPositions.Count; i++)
+{
+    Console.WriteLine($"Switch {i + 1}: {switchPositions[i]}");
+}
+
+Console.WriteLine();
+Console.WriteLine("Press enter to continue...");
+Console.ReadLine();
