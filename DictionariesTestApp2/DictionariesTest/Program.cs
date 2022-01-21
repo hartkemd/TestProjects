ba1d9fe0f32103881161665c90b78e9664920d6b
hartkemd <hartkemd@gmail.com>
2022-01-21 18:14:48 -0500

    Added Dictionaries test projects.

---
+++ DictionariesTestApp2/DictionariesTest/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Dictionary<int, string> employees = new Dictionary<int, string>();
+employees.Add(1, "Mark");
+employees.Add(2, "Tim");
+employees.Add(3, "Sue");
+employees.Add(4, "Bob");
+
+Console.Write("Please enter your employee ID: ");
+string idString = Console.ReadLine();
+bool isValidId = int.TryParse(idString, out int id);
+
+if (isValidId == true && id > 0 && id < 5)
+{
+    Console.WriteLine($"Your name is: {employees[id]}");
+}
+else
+{
+    Console.WriteLine("That was not a valid ID");
+}
+
+Console.WriteLine();
+Console.WriteLine("Press enter to continue...");
+Console.ReadLine();
---
+++ DictionariesTestApp3/DictionariesTest/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Dictionary<string, string> definitions = new Dictionary<string, string>();
+definitions.Add("C", "a programming language developed at Bell Labs by Ken Thompson and Dennis Ritchie.");
+definitions.Add("C++", "a programming language developed at Bell Labs by Bjarne Stroustrup.");
+definitions.Add("C#", "a programming language developed at Microsoft by Anders Hejlsberg.");
+
+//Console.WriteLine(definitions["B"]);
+Console.WriteLine($"C is: {definitions["C"]}");
+Console.WriteLine($"C++ is: {definitions["C++"]}");
+Console.WriteLine($"C# is: {definitions["C#"]}");
+
+Console.WriteLine();
+Console.WriteLine("Press enter to continue...");
+Console.ReadLine();
