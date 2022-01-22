f4e7bfa74b80785d9bb0e5ebe4f9e7256b18beb9
hartkemd <hartkemd@gmail.com>
2022-01-22 18:25:03 -0500

    Added exceptions test projects.

---
+++ ExceptionsTestApp3/ExceptionsTest/Program.cs
@@ -0,0 +1,32 @@
+﻿
+try
+{
+    ThrowException();
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("This is from the finally block.");
+}
+
+Console.WriteLine();
+Console.WriteLine("Press enter to continue...");
+Console.ReadLine();
+
+static void ThrowException()
+{
+    int total = 0;
+
+    for (int i = 4; i >= 0; i--)
+    {
+        total += 10 / i;
+        Console.WriteLine(total);
+    }
+}
