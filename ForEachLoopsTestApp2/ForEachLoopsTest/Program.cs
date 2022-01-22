95324c36fcb15eff48580e897687a001013b9ebd
hartkemd <hartkemd@gmail.com>
2022-01-21 19:21:33 -0500

    Added foreach loop test projects.

---
+++ ForEachLoopsTestApp2/ForEachLoopsTest/Program.cs
@@ -0,0 +1,24 @@
+﻿
+string firstName;
+List<string> firstNames = new List<string>();
+
+do
+{
+    Console.Write("What is your first name? (enter [done] to quit) ");
+    firstName = Console.ReadLine();
+    firstNames.Add(firstName);
+} while (firstName.ToLower() != "done");
+
+Console.WriteLine();
+
+foreach (var name in firstNames)
+{
+    if (name.ToLower() != "done")
+    {
+        Console.WriteLine($"Hello, {name}");
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Press enter to continue...");
+Console.ReadLine();
---
+++ ForEachLoopsTestApp3/ForEachLoopsTest/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Dictionary<int, decimal> prices = new Dictionary<int, decimal>();
+prices.Add(2015, 2.00M);
+prices.Add(2016, 2.25M);
+prices.Add(2017, 2.25M);
+prices.Add(2018, 2.30M);
+prices.Add(2019, 3.00M);
+prices.Add(2020, 2.50M);
+
+foreach (var price in prices)
+{
+    Console.WriteLine($"The price of a bag of chips was: ${price.Value} in {price.Key}");
+}
+
+Console.WriteLine();
+Console.WriteLine("Press enter to continue...");
+Console.ReadLine();
