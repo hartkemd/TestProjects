a2247398f36e76732f237738dbca6da9bdf13203
hartkemd <hartkemd@gmail.com>
2022-01-22 14:56:19 -0500

    Added breakpoints test projects.

---
+++ BreakpointsTestApp3/BreakpointsTest/Program.cs
@@ -0,0 +1,16 @@
+﻿
+for (int i = 0; i < 11; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine($"{i} is divisible by 2");
+    }
+    if (i % 3 == 0)
+    {
+        Console.WriteLine($"{i} is divisible by 3");
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Press enter to continue...");
+Console.ReadLine();
