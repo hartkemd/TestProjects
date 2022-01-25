50f1efb53f40babf4f107f7b5972a9348912fc48
hartkemd <hartkemd@gmail.com>
2022-01-24 21:09:45 -0500

    Added namespaces test projects.

---
+++ NamespacesTestApp1/NamespacesTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using FoundationInfo.Calculators;
+using NamespacesTest.Models;
+
+namespace NamespacesTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            PersonModel person = new PersonModel();
+            double result = Calculations.Add(1.5, 1.6);
+            Console.WriteLine(result);
+
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NamespacesTestApp2/NamespacesTest/Calculators/BasicCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamespacesTest.Calculators
+{
+    public static class BasicCalculator
+    {
+        public static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+    }
+}
