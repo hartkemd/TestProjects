a35586a1f9d35909e2b5ce9530703127fd32ae94
hartkemd <hartkemd@gmail.com>
2022-02-27 12:01:11 -0500

    Added Tuples test projects.

---
+++ TuplesTestApp2/ConsoleUI/Program.cs
@@ -0,0 +1,30 @@
+﻿
+List<int> numbers = new List<int>{ 1, 2, 3, 4 };
+
+(int min, int max) = GetMinAndMax(numbers);
+
+Console.WriteLine($"The minimum is: {min} and the maximum is: {max}");
+
+Console.WriteLine("Press enter to continue...");
+Console.ReadLine();
+
+(int, int) GetMinAndMax(List<int> numbers)
+{
+    int min = numbers[0];
+    int max = numbers[0];
+
+    foreach (int number in numbers)
+    {
+        if (number < min)
+        {
+            min = number;
+        }
+
+        if (number > max)
+        {
+            max = number;
+        }
+    }
+
+    return (min, max);
+}
