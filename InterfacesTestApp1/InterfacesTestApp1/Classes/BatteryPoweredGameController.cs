eacf468cf5f8a666b785365467a5dd8de65e2e25
hartkemd <hartkemd@gmail.com>
2022-05-17 15:49:03 -0400

    Added first Interfaces test app.

---
+++ InterfacesTestApp1/InterfacesTestApp1/Classes/BatteryPoweredGameController.cs
@@ -0,0 +1,14 @@
+﻿using InterfacesTestApp1.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesTestApp1.Classes
+{
+    public class BatteryPoweredGameController : GameController, IBatteryPowered
+    {
+        public int BatteryLevel { get; set; }
+    }
+}
---
+++ InterfacesTestApp1/InterfacesTestApp1/Interfaces/IBatteryPowered.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesTestApp1.Interfaces
+{
+    public interface IBatteryPowered
+    {
+        public int BatteryLevel { get; set; }
+    }
+}
