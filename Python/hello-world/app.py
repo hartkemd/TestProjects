2a7901fe04e1389930ca0603d1f50fcb226d7624
hartkemd <hartkemd@gmail.com>
2023-03-19 09:06:40 -0400

    add Python

---
+++ Python/hello-world/app.py
@@ -0,0 +1,6 @@
+print('Hello, world!')
+print('*' * 20)
+x = 1
+
+first_name = "Mark"
+print(first_name.lower())
---
+++ Python/selenium/app.py
@@ -0,0 +1,42 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def test_eight_components():
+    driver = webdriver.Edge()
+
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
+    title = driver.title
+    assert title == "Web form"
+
+    time.sleep(4)
+
+    text_box = driver.find_element(by=By.NAME, value="my-text")
+    submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+
+    text_box.send_keys("Selenium")
+    time.sleep(4)
+    submit_button.click()
+
+    message = driver.find_element(by=By.ID, value="message")
+    value = message.text
+    assert value == "Received!"
+
+    time.sleep(4)
+    driver.quit()
+
+test_eight_components()
+
+# driver = webdriver.Edge()
+
+# driver.get('https://bing.com')
+
+# time.sleep(4)
+# element = driver.find_element(By.ID, 'sb_form_q')
+# element.send_keys('WebDriver')
+# element.submit()
+
+# time.sleep(4)
+# driver.quit()
