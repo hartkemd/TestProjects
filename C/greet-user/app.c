31591a86f2dc42f0ea72b7e4d6d6d52cee4ea010
Mark Hartkemeyer <hartkemd@gmail.com>
2023-11-12 09:53:15 -0500

    Implement removeWhitespace function

--- C/greet-user/app.c
+++ C/greet-user/app.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 const int maxCharsInName = 20;
 
 void getNameFromUser(char *name);
-void removeNewlineChar(char input[]);
+void removeNewlineChar(char input[], size_t len);
+void removeWhitespace(char *input);
+bool inputTooLong(char input[], size_t len, int maxLength);
 void greetUser(char name[]);
-bool inputTooLong(char input[], int maxLength);
 
 int main() {
 	char name[maxCharsInName];
@@ -19,40 +21,61 @@ int main() {
 }
 
 void getNameFromUser(char *name) {
-	int len = 0;
+	size_t len = 0;
 	char input[50];
 	while(len == 0 || len >= maxCharsInName) {
 		printf("Please enter your first name: ");
 		fgets(input, sizeof(input), stdin);
-		removeNewlineChar(input);
+		len = strlen(input);
+		removeNewlineChar(input, len);
+        removeWhitespace(input);
 		len = strlen(input);
 
 		if (len == 0)
             printf("Name cannot be empty. Please try again.\n");
-
-        if (inputTooLong(input, maxCharsInName) == true) {
+        else if (inputTooLong(input, len, maxCharsInName) == true)
             printf("The maximum characters allowed for a name is %d. Please try again.\n", maxCharsInName);
-        }
 	}
 
 	strcpy(name, input);
 }
 
-void removeNewlineChar(char input[]) {
+void removeNewlineChar(char input[], size_t len) {
 	// Replace newline character at end of string with null
-	size_t len = strlen(input);
 	if (len > 0 && input[len - 1] == '\n') {
 		input[len - 1] = '\0';
 	}
 }
 
-bool inputTooLong(char input[], int maxLength) {
-	size_t len = strlen(input);
+void removeWhitespace(char *input) {
+
+    // Base case: If the string is empty, return
+    if (*input == '\0') {
+        return;
+    }
+
+    // If the current character is a whitespace character, remove it
+    if ((isspace(*input) != 0)) {
+        // Shift all characters to the left
+        char *temp = input;
+        while (*temp != '\0') {
+            *temp = *(temp + 1);
+            temp++;
+        }
+
+        // Call the function recursively on the modified string
+        removeWhitespace(input);
+    } else {
+        // Move to the next character in the string
+        removeWhitespace(input + 1);
+    }
+}
+
+bool inputTooLong(char input[], size_t len, int maxLength) {
 	if (len > maxLength) return true;
 	else return false;
 }
 
 void greetUser(char *name) {
-	size_t len = strlen(name);
 	printf("Hello, %s!\n", name);
 }
